e99d6df7800f3753f9d6725e4ea440c2c469cef8
David McCullars <david@bloomfire.com>
2020-01-02 09:49:36 -0600

    Bump PostgreSQL to 11.5

--- Formula/postgresql.rb
+++ Formula/postgresql.rb
@@ -15,9 +15,6 @@ class Postgresql < Formula
   depends_on "readline"
   depends_on "python" => :optional
 
-  conflicts_with "postgres-xc",
-    :because => "postgresql and postgres-xc install the same binaries."
-
   def install
     # avoid adding the SDK library directory to the linker search path
     ENV["XML2_CONFIG"] = "xml2-config --exec-prefix=/usr"
